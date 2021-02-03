148d84862b9f9a3f58dd65628cc9d5c777108791
jobayer-h <jobayer.dev@gmail.com>
2021-02-04 00:57:06 +0600

    appoinment post to server

--- src/components/Appoinmentpage/AppoinmentServiceBox.js
+++ src/components/Appoinmentpage/AppoinmentServiceBox.js
@@ -1,11 +1,12 @@
 
-import React, { useState } from 'react';
+import React, { useContext, useState } from 'react';
 import { Button, Modal, ModalBody, ModalFooter, ModalTitle } from 'react-bootstrap';
 import ModalHeader from 'react-bootstrap/esm/ModalHeader';
+import { userContext } from '../../App';
 import './appoinment.css'
 
 const AppoinmentServiceBox = ({ data }) => {
-
+    const [loggedInUser, setLoggedInUser] = useContext(userContext);
     const [show, setShow] = useState(false);
 
     const handleClose = () => setShow(false);
@@ -27,32 +28,31 @@ const AppoinmentServiceBox = ({ data }) => {
                     centered
                 >
                     <ModalHeader closeButton>
-                        <ModalTitle>{data.name}</ModalTitle>
+                        <h3 className="brand-text text-center">{data.name}</h3>
                     </ModalHeader>
                     <ModalBody>
-                        <form>
+                        <form action="http://localhost:4000/addAppoinment" method="post">
                             <div class="form-group">
-                                
+                                <input type="text" class="form-control" id="service" name="service" placeholder="Service" Value={data.name} />
                                 <input type="time" class="form-control" id="time" name="time" placeholder="Time" />
-                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" />
-                                <input type="number" class="form-control" id="phone" name="phone" placeholder="Phone Number" />
-                                <input type="email" class="form-control" id="email" name="email" placeholder="Email" />
+                                <input type="text" class="form-control" id="name" name="name" placeholder="Name" Value={loggedInUser.displayName} />
+                                <input type="text" class="form-control" id="phone" name="phone" placeholder="Phone Number" />
+                                <input type="email" class="form-control" id="email" name="email" placeholder="Email" Value={loggedInUser.email} />
                                 <input type="date" class="form-control" id="date" name="date" placeholder="Date" />
-
+                                <input type="submit" className="btn btn-brand" value="SEND" />
                             </div>
-                            
                         </form>
                     </ModalBody>
-                    <ModalFooter>
-                        <Button variant="secondary" onClick={handleClose}>
-                            Close
-                    </Button>
-                        <Button variant="primary">Understood</Button>
-                    </ModalFooter>
                 </Modal>
             </div>
         </div>
     );
 };
+
+
+// <ModalFooter>
+// <Button variant="secondary" onClick={handleClose}>Close</Button>
+// <Button variant="primary">Understood</Button>
+// </ModalFooter>
 
 export default AppoinmentServiceBox;
